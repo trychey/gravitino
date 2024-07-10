9658ef6efa5e378cfbc6dba3b98fdf119f9e3ef9
cancai <cancai@xiaomi.com>
2024-07-10 19:10:19 +0800

    feat: (catalogs-lakehouse-paimon): Support hdfs kerberos auth for Paimon

--- catalogs/catalog-lakehouse-paimon/build.gradle.kts
+++ catalogs/catalog-lakehouse-paimon/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 val scalaVersion: String = project.properties["scalaVersion"] as? String ?: extra["defaultScalaVersion"].toString()
-val sparkVersion: String = libs.versions.spark34.get()
+val sparkVersion: String = libs.versions.spark.get()
 val sparkMajorVersion: String = sparkVersion.substringBeforeLast(".")
 val paimonVersion: String = libs.versions.paimon.get()
 
@@ -25,6 +25,7 @@ dependencies {
   }
   implementation(libs.bundles.log4j)
   implementation(libs.commons.lang3)
+  implementation(libs.caffeine)
   implementation(libs.guava)
   implementation(libs.hadoop2.common) {
     exclude("com.github.spotbugs")
@@ -124,6 +125,7 @@ tasks.test {
 
     doFirst {
       environment("GRAVITINO_CI_HIVE_DOCKER_IMAGE", "datastrato/gravitino-ci-hive:0.1.12")
+      environment("GRAVITINO_CI_KERBEROS_HIVE_DOCKER_IMAGE", "datastrato/gravitino-ci-kerberos-hive:0.1.3")
     }
 
     val init = project.extra.get("initIntegrationTest") as (Test) -> Unit
--- catalogs/catalog-lakehouse-paimon/src/test/java/com/datastrato/gravitino/catalog/lakehouse/paimon/integration/test/CatalogPaimonBaseIT.java
+++ catalogs/catalog-lakehouse-paimon/src/test/java/com/datastrato/gravitino/catalog/lakehouse/paimon/integration/test/CatalogPaimonBaseIT.java
@@ -1,6 +1,20 @@
 /*
- * Copyright 2024 Datastrato Pvt Ltd.
- * This software is licensed under the Apache License version 2.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package com.datastrato.gravitino.catalog.lakehouse.paimon.integration.test;
 
@@ -12,6 +26,7 @@ import com.datastrato.gravitino.SchemaChange;
 import com.datastrato.gravitino.SupportsSchemas;
 import com.datastrato.gravitino.catalog.lakehouse.paimon.PaimonCatalogPropertiesMetadata;
 import com.datastrato.gravitino.catalog.lakehouse.paimon.PaimonConfig;
+import com.datastrato.gravitino.catalog.lakehouse.paimon.ops.PaimonBackendCatalogWrapper;
 import com.datastrato.gravitino.catalog.lakehouse.paimon.utils.CatalogUtils;
 import com.datastrato.gravitino.client.GravitinoMetalake;
 import com.datastrato.gravitino.dto.util.DTOConverters;
@@ -511,7 +526,9 @@ public abstract class CatalogPaimonBaseIT extends AbstractIT {
     Preconditions.checkArgument(
         StringUtils.isNotBlank(type), "Paimon Catalog backend type can not be null or empty.");
     catalogProperties.put(PaimonCatalogPropertiesMetadata.PAIMON_METASTORE, type);
-    paimonCatalog = CatalogUtils.loadCatalogBackend(new PaimonConfig(catalogProperties));
+    PaimonBackendCatalogWrapper paimonBackendCatalogWrapper =
+        CatalogUtils.loadCatalogBackend(new PaimonConfig(catalogProperties));
+    paimonCatalog = paimonBackendCatalogWrapper.getCatalog();
   }
 
   private void createSchema() {
